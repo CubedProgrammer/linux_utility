5074a5a77b03a8a344de7eb584985ce65de665df
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2023-03-22 16:53:28 +0000

    Fixed jhash and unic.

--- jhash.c
+++ jhash.c
@@ -1,9 +1,37 @@
 #include<stdio.h>
 int jhash(const char *it)
 {
+    unsigned ch;
     unsigned h = 0;
     for(; *it != '\0'; ++it)
-        h = h * 31 + *it;
+    {
+        ch = *it;
+        ch &= 255;
+        if(ch > 239)
+        {
+            ch = (it[0] & 7) << 18 | (it[1] & 63) << 12 | (it[2] & 63) << 6 | it[3] & 63;
+            it += 3;
+        }
+        else if(ch > 223)
+        {
+            ch = (it[0] & 15) << 12 | (it[1] & 63) << 6 | it[2] & 63;
+            it += 2;
+        }
+        else if(ch > 191)
+        {
+            ch = (it[0] & 31) << 6 | it[1] & 63;
+            ++it;
+        }
+        h *= 31;
+        if(ch > 0xffff)
+        {
+            h += ch >> 10 | 0xd800;
+            h *= 31;
+            ch &= 0x3ff;
+            ch |= 0xdc00;
+        }
+        h += ch;
+    }
     return h;
 }
 int main(int argl, char *argv[])
--- unic.c
+++ unic.c
@@ -6,7 +6,7 @@ int main(int argl, char *argv[])
     {
         arg = argv[i];
         for(const char *it = arg; *it != 0; ++it)
-            printf("%02x", *it);
+            printf("%02x", (unsigned)*it & 0xff);
         putchar('\n');
     }
     return argl == 1;
