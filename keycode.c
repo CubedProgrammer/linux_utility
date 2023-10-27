0c1f2a71b839e6d978891685bc4ba6594f2082ab
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2023-10-27 21:34:38 +0000

    Fixed keycode for certain special keys.

--- keycode.c
+++ keycode.c
@@ -4,12 +4,15 @@
 int main(int argl, char *argv[])
 {
     unsigned char cbuf[8];
+    char outbuf[17];
+    const char digitarr[] = "0123456789ABCDEF";
     char exitch = 27;
     unsigned len = 0;
     struct termios old, curr;
     tcgetattr(STDIN_FILENO, &old);
     curr = old;
-    curr.c_lflag &= ~(ECHO | ICANON);
+    curr.c_iflag &= ~(ICRNL | IXON);
+    curr.c_lflag &= ~(ECHO | ICANON | ISIG);
     tcsetattr(STDIN_FILENO, TCSANOW, &curr);
     if(argl > 1)
         exitch = argv[1][0];
@@ -17,8 +20,12 @@ int main(int argl, char *argv[])
     {
         len = read(STDIN_FILENO, cbuf, sizeof cbuf);
         for(unsigned i = 0; i < len; ++i)
-            printf("%02X", (unsigned)cbuf[i]);
-        putchar('\n');
+        {
+            outbuf[i << 1] = digitarr[cbuf[i] >> 4];
+            outbuf[(i << 1) + 1] = digitarr[cbuf[i] & 15];
+        }
+        outbuf[len << 1] = '\n';
+        fwrite(outbuf, 1, (len << 1) + 1, stdout);
     }
     tcsetattr(STDIN_FILENO, TCSANOW, &old);
     return 0;
