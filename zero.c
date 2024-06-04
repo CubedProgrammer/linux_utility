c18a5994fb7eaf7d01d2dc255d1f031a8420574a
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2024-06-04 22:53:03 +0000

    Zero specifiy size for file.

--- zero.c
+++ zero.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<sys/stat.h>
 char zero[8192];
 int main(int argl, char *argv[])
@@ -10,11 +11,16 @@ int main(int argl, char *argv[])
     {
         struct stat fdat;
         size_t fsz, cnt;
+        char*filename;
         int ret;
         FILE *fh;
         for(int i = 1; i < argl; ++i)
         {
-            ret = stat(argv[i], &fdat) * -1;
+            fsz = 0;
+            filename = argv[i];
+            if(argv[i][0] == '-')
+                fsz = strtoul(argv[i] + 1, &filename, 10);
+            ret = stat(filename, &fdat) * -1;
             if(ret)
             {
                 fprintf(stderr, "stat %s", argv[i]);
@@ -22,7 +28,7 @@ int main(int argl, char *argv[])
             }
             else
             {
-                fh = fopen(argv[i], "wb");
+                fh = fopen(filename, "wb");
                 if(fh == NULL)
                 {
                     fprintf(stderr, "%s", argv[i]);
@@ -30,7 +36,8 @@ int main(int argl, char *argv[])
                 }
                 else
                 {
-                    fsz = fdat.st_size;
+                    if(fsz == 0)
+                        fsz = fdat.st_size;
                     cnt = 0;
                     fwrite(zero, 1, fsz & sizeof(zero) - 1, fh);
                     fsz &= -sizeof(zero);
